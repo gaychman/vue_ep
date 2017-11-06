2ff511605a8a750edf0b3881f1bb1a424bc6fa62
gaychman <dmitry.gaychman@gmail.com>
2017-11-06 16:19:45 +0300

    Правильные иконки в панели управления экраном

--- VueEP/src/components/course-builder/ScreenEditor.vue
+++ VueEP/src/components/course-builder/ScreenEditor.vue
@@ -3,14 +3,17 @@
         <md-toolbar class="md-transparent">
             <md-button class="md-icon-button">
                 <md-icon>undo</md-icon>
+                <md-tooltip>Отменить</md-tooltip>
             </md-button>
             <md-button class="md-icon-button">
                 <md-icon>redo</md-icon>
+                <md-tooltip>Вернуть</md-tooltip>
             </md-button>
 
             <md-menu md-size="4" md-direction="top right" ref="menu">
                 <md-button class="md-icon-button" md-menu-trigger>
                     <md-icon>add</md-icon>
+                    <md-tooltip>Создать объект</md-tooltip>
                 </md-button>
 
                 <md-menu-content>
@@ -20,13 +23,16 @@
                 </md-menu-content>
             </md-menu>
             <md-button class="md-icon-button">
-                <md-icon>cut</md-icon>
+                <md-icon>content_cut</md-icon>
+                <md-tooltip>Вырезать</md-tooltip>
             </md-button>
             <md-button class="md-icon-button">
-                <md-icon>copy</md-icon>
+                <md-icon>content_copy</md-icon>
+                <md-tooltip>Копировать</md-tooltip>
             </md-button>
             <md-button class="md-icon-button">
-                <md-icon>paste</md-icon>
+                <md-icon>content_paste</md-icon>
+                <md-tooltip>Вставить</md-tooltip>
             </md-button>
         </md-toolbar>
 
